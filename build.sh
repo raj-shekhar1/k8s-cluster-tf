79cd14972cc21a5d4afc9313fdceeeca454fb71f
Raj Shekhar <raj@infinichains.com>
2019-09-23 11:01:27 +0530

    Modified build scripts and comments in README

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 cd raj-shekhar1/k8s-cluster-tf
-#bundle install
-#bundle exec jekyll build
+
 cd -
 
 ./terraform-linux init
-#./terraform-linux validate website
+
 
 if [[ $TRAVIS_BRANCH == 'master' ]]
 then
-   # ./terraform-linux workspace select prod
     ./terraform-linux apply -auto-approve
 fi
